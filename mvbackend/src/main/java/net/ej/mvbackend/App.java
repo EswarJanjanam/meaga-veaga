9546cdce5450ddcaa8d61b5214d5edc17b6d6504
Leela <Leela@ESWAR-PC>
2019-01-05 12:31:01 +0530

    Intial setup

---
+++ mvbackend/src/main/java/net/ej/mvbackend/App.java
@@ -0,0 +1,13 @@
+package net.ej.mvbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
